29740983db6cc93c72c9531c1f97f2fe3f77c8a9
okunotomoya <gomakuro25@gmail.com>
2025-08-24 20:57:25 +0900

    Create run script for model v3

---
+++ scripts/train_v2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+python -m adtecplayground.train \
+--model_name "tohoku-nlp/bert-base-japanese-v2" \
+--dataset_name "cyberagent/AdTEC" \
+--dataset_config "ad-acceptability" \
+--max_length 128 \
+--seeds 0 1 2 3 4 5 6 7 8 9 \
+--learning_rates 2e-5 5.5e-5 2e-6 \
+--num_train_epochs 30 \
+--patience 3 \
+--batch_size 32 \
+--warmup_ratio 0.0 \
+--weight_decay 0.0 \
+--gradient_accumulation_steps 1 \
+--output_dir ./runs/adtec-bert-v2 \
+--fp16
--- scripts/train_v3.sh
+++ scripts/train_v3.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 
 python -m adtecplayground.train \
---model_name "tohoku-nlp/bert-base-japanese-v2" \
+--model_name "tohoku-nlp/bert-base-japanese-v3" \
 --dataset_name "cyberagent/AdTEC" \
 --dataset_config "ad-acceptability" \
 --max_length 128 \
---seeds 0 \
+--seeds 0 1 2 3 4 5 6 7 8 9 \
 --learning_rates 2e-5 5.5e-5 2e-6 \
 --num_train_epochs 30 \
 --patience 3 \
@@ -15,5 +15,5 @@ python -m adtecplayground.train \
 --warmup_ratio 0.0 \
 --weight_decay 0.0 \
 --gradient_accumulation_steps 1 \
---output_dir ./runs/adtec-bert-v2 \
+--output_dir ./runs/adtec-bert-v3 \
 --fp16
